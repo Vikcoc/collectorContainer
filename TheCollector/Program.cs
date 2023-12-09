9e32b86d3ba1591e93c15a30ed9ff2659e3acfef
Vikcoc <vikcoc@yahoo.com>
2023-12-09 21:24:18 +0200

    extra spaces

--- TheCollector/Program.cs
+++ TheCollector/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json.Linq;
 using Npgsql;
 using OWT.CryptoCom.Dto;
 using OWT.SocketClient;
